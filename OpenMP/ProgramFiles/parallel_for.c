bf2b4977a0de10477df2443c10f8c1c0e42e028a
GavinWz <gavinwzmails@gmail.com>
2020-02-06 17:17:47 +0800

    parallel for

--- OpenMP/ProgramFiles/parallel_for.c
+++ OpenMP/ProgramFiles/parallel_for.c
@@ -9,19 +9,25 @@ int main(){
     int i;
     double start = omp_get_wtime();
 
-    // clock_t start = clock();
     for(i = 0; i < 2; i++)
         sum();
     double end = omp_get_wtime();
-    // clock_t end = clock();
     printf("Running 2 times spent without parallel: %lf\n", end - start);
 
-    // start = clock();
     start = omp_get_wtime();
     #pragma omp parallel for
     for(i = 0; i < 2; i++)
         sum();
-    // end = clock();
+    end = omp_get_wtime();
+    printf("Running 2 times spent  with   parallel: %lf\n", end - start);
+
+    start = omp_get_wtime();
+    #pragma omp parallel  //the other way to program
+    {
+        #pragma omp for
+        for(i = 0; i < 2; i++)
+            sum();
+    }
     end = omp_get_wtime();
     printf("Running 2 times spent  with   parallel: %lf\n", end - start);
 
@@ -29,13 +35,11 @@ int main(){
 
 void sum(){
     double sum = 0;
-    // clock_t start = clock();
     double start = omp_get_wtime();
     for(int i = 0; i < 10000000; i++){
         sum++;
 
     }
-    // clock_t end = clock();
     double end = omp_get_wtime();
     printf("Every time spend: %lf\n", end - start);
 
