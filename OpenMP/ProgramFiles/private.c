18ba47b9ecd21c58b69ce8d73ca2dd9e1f3eda25
GavinWz <gavinwzmails@gmail.com>
2020-02-06 20:43:44 +0800

    private

--- OpenMP/ProgramFiles/private.c
+++ OpenMP/ProgramFiles/private.c
@@ -2,41 +2,32 @@
 #include<omp.h>
 
 int main(){
+    /*private*/
     int num = 20;
-    #pragma omp parallel private(num) num_threads(2)
-    {
-        if(omp_get_thread_num() == 0){
-            if(num == 20)
-                printf("in private, num hasn't changed.\n");
-            else{
-                printf("in private num hasn's initialized.\n");
-                num = 0;
-            }
-        }
-        #pragma omp parallel for
-        for(int i = 0; i < 2; i++){
-            num += i;
-            printf("num = %d\n", num);
-        }
+    #pragma omp parallel for private(num) num_threads(2)
+    for(int i = 0; i < 2; i++){
+        num += i;
+        printf("num = %d\n", num);
     }
+    
     printf("For private num, after parallel, num = %d.\n", num);
 
+    /*firstprivate*/
     num = 20;
-    #pragma omp parallel firstprivate(num) num_threads(2)
-    {
-        if(omp_get_thread_num() == 0){
-            if(num == 20)
-                printf("in firstprivatem, num hasn't changed.\n");
-            else{
-                printf("in firstprivatem, num hasn's initialized.\n");
-                num = 0;
-            }
-        }
-        #pragma omp parallel for
-        for(int i = 0; i < 2; i++){
-            num += i;
-            printf("num = %d\n", num);
-        }
+    #pragma omp parallel for firstprivate(num) num_threads(2)
+    for(int i = 0; i < 2; i++){
+        num += i;
+        printf("num = %d\n", num);
     }
     printf("For first private num, after parallel, num = %d.\n", num);
+
+    /*lastprivate*/
+    num = 20;
+    #pragma omp parallel for firstprivate(num), lastprivate(num) num_threads(2)
+    for(int i = 0; i < 2; i++){
+        num += i;
+        printf("num = %d\n", num);
+    }
+    
+    printf("For last private num, after parallel, num = %d.\n", num);
 }
