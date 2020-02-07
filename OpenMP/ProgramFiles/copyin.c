214a1e3f2fa3378dd37ac0a6134a1a95f79323f2
GavinWz <gavinwzmails@gmail.com>
2020-02-07 20:45:23 +0800

    copyin

---
+++ OpenMP/ProgramFiles/copyin.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<omp.h>
+
+int g = 10;
+#pragma omp threadprivate(g)
+
+int main(){
+    printf("Global g = %d\n", g);
+    #pragma omp parallel for copyin(g)
+    for(int i = 0; i < 4; i++){
+        g += i;
+        printf("thread %d, g = %d\n", omp_get_thread_num(), g);
+    }
+    printf("After parallel operate, Global g = %d\n", g);
+
+
+    
+    
+}
