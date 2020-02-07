7d7be2a3d61c0bd815469720732f85df710ad2c6
GavinWz <gavinwzmails@gmail.com>
2020-02-07 21:54:08 +0800

    in_parallel

---
+++ OpenMP/ProgramFiles/in_parallel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", omp_in_parallel());
+	omp_set_num_threads(4);
+	#pragma omp parallel  
+	{
+	    // YOUR CODE HERE
+		printf("%d\n", omp_in_parallel());
+	    // END OF YOUR CODE
+	}
+	return 0;
+}
