005253db41c19d481637cab8a58726b300342acc
GavinWz <gavinwzmails@gmail.com>
2020-02-07 21:51:14 +0800

    num_threads

--- OpenMP/ProgramFiles/num_threads.c
+++ OpenMP/ProgramFiles/num_threads.c
@@ -4,6 +4,11 @@
 int main(){
     #pragma omp parallel num_threads(4)
     {
-        printf("I'm thread %d.\n", omp_get_thread_num());
+        printf("I'm thread %d of %d.\n", 
+            omp_get_thread_num(), omp_get_num_threads());
+        printf("Now, there are %d avalibal threads.\n", 
+            omp_get_num_procs());
+    
     }
+
 }
