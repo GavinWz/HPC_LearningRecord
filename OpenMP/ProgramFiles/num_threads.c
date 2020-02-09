d97b5a71616da8f644eddbfc44179b6bb3395cac
GavinWz <gavinwzmails@gmail.com>
2020-02-09 10:04:16 +0800

    num of threads

--- OpenMP/ProgramFiles/num_threads.c
+++ OpenMP/ProgramFiles/num_threads.c
@@ -2,13 +2,15 @@
 #include<omp.h>
 
 int main(){
+
+    printf("The max number of threads is: %d\n", omp_get_max_threads());
+
     #pragma omp parallel num_threads(4)
     {
         printf("I'm thread %d of %d.\n", 
             omp_get_thread_num(), omp_get_num_threads());
         printf("Now, there are %d avalibal threads.\n", 
             omp_get_num_procs());
-    
     }
 
 }
